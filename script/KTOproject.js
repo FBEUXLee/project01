c7de781d527fc0867bf1d904e8060836b520f963
FBEUXLee <dlwlgur37@gmail.com>
2022-11-20 23:50:19 +0900

    KTO

--- script/KTOproject.js
+++ script/KTOproject.js
@@ -14,7 +14,7 @@ var swiper = new Swiper(".swiper.banner", {
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
-      },
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -23,7 +23,7 @@ var swiper = new Swiper(".swiper.banner", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
+    },
 });
 
 $(function () {
@@ -37,39 +37,39 @@ $(function () {
     });
     // 섹션2 hover
     $(".boxContent01 a:eq(0)").hover(function () {
-        $(this).children('img').attr("src", "/project01/image/채용정보2.svg");
+        $(this).children('img').attr("src", "image/채용정보2.svg");
     }, function () {
-        $(this).children('img').attr("src", "/project01/image/채용정보.svg");
+        $(this).children('img').attr("src", "image/채용정보.svg");
     });
     $(".boxContent01 a:eq(1)").hover(function () {
-        $(this).children('img').attr("src", "/project01/image/알림마당2.svg");
+        $(this).children('img').attr("src", "image/알림마당2.svg");
     }, function () {
-        $(this).children('img').attr("src", "/project01/image/알림마당.svg");
+        $(this).children('img').attr("src", "image/알림마당.svg");
     });
     $(".boxContent01 a:eq(2)").hover(function () {
-        $(this).children('img').attr("src", "/project01/image/사업공고공모2.svg");
+        $(this).children('img').attr("src", "image/사업공고공모2.svg");
     }, function () {
-        $(this).children('img').attr("src", "/project01/image/사업공고공모.svg");
+        $(this).children('img').attr("src", "image/사업공고공모.svg");
     });
     $(".boxContent01 a:eq(3)").hover(function () {
-        $(this).children('img').attr("src", "/project01/image/정보공개신청2.svg");
+        $(this).children('img').attr("src", "image/정보공개신청2.svg");
     }, function () {
-        $(this).children('img').attr("src", "/project01/image/정보공개신청.svg");
+        $(this).children('img').attr("src", "image/정보공개신청.svg");
     });
     $(".boxContent01 a:eq(4)").hover(function () {
-        $(this).children('img').attr("src", "/project01/image/영상물지원요청2.svg");
+        $(this).children('img').attr("src", "image/영상물지원요청2.svg");
     }, function () {
-        $(this).children('img').attr("src", "/project01/image/영상물지원요청.svg");
+        $(this).children('img').attr("src", "image/영상물지원요청.svg");
     });
     $(".boxContent01 a:eq(5)").hover(function () {
-        $(this).children('img').attr("src", "/project01/image/관광정보수정신규요청2.svg");
+        $(this).children('img').attr("src", "image/관광정보수정신규요청2.svg");
     }, function () {
-        $(this).children('img').attr("src", "/project01/image/관광정보수정신규요청.svg");
+        $(this).children('img').attr("src", "image/관광정보수정신규요청.svg");
     });
     $(".boxContent01 a:eq(6)").hover(function () {
-        $(this).children('img').attr("src", "/project01/image/똑똑한문의2.svg");
+        $(this).children('img').attr("src", "image/똑똑한문의2.svg");
     }, function () {
-        $(this).children('img').attr("src", "/project01/image/똑똑한문의.svg");
+        $(this).children('img').attr("src", "image/똑똑한문의.svg");
     });
 
     // 섹션3 right 보도자료
@@ -114,6 +114,7 @@ $(function () {
     $(".sitemap .close").click(function () {
         $(".sitemap").hide();
     });
+
     var $gnb = $('.gnb'),
         $gnb_top = $gnb.find('.depth1 > .gnb_item');
 
