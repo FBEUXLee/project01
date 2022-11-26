5da9489d83d23cfeddd747ba78e079309fb0b23a
FBEUXLee <dlwlgur37@gmail.com>
2022-11-26 17:00:07 +0900

    KTO

--- script/index.js
+++ script/index.js
@@ -1,5 +1,11 @@
 $(function(){
-    $("header nav .navi li").click(function(e,index){
-        e.preventDefault();     
-    })
+    $('nav ul li').on('click', function(){
+        $('.vertical-slider').slick('slickGoTo', $(this).data('index'));
+    });
+    const swiper = new Swiper('.swiper', {
+        mousewheel: {
+          invert: true,
+          sensetivity : 1
+        },
+      });
 });
