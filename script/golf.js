ea25765136f20a3fe792fb92842d949c787c4398
FBEUXLee <dlwlgur37@gmail.com>
2022-12-06 18:10:36 +0900

    golf

--- script/golf.js
+++ script/golf.js
@@ -1,5 +1,5 @@
 $(function(){
-    $(".gnb .depth1 > .gnb_item > .gnb_anchor").hover(function(){
+    $(".gnb .depth1 > .gnb_item").hover(function(){
         $(".gnb .depth2").stop().slideToggle();
     });
 });
