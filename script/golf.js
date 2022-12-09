9ed6a22a7e41a0ee5916bc6d0f39b434f90cfd65
FBEUXLee <dlwlgur37@gmail.com>
2022-12-09 15:11:55 +0900

    golf

--- script/golf.js
+++ script/golf.js
@@ -3,7 +3,7 @@ $(function () {
         $(".gnb .depth2").stop().slideToggle();
     });
 
-    // 골프장날씨
+    // 골프장 날씨
     fetch('http://goweather.herokuapp.com/weather/Seogwipo')
         .then((response) => response.json())
         .then((data) => {
@@ -11,4 +11,11 @@ $(function () {
             document.getElementById("wind").innerHTML = data['wind'];
             document.getElementById("air").innerHTML = data['description'];
         });
+    
+    //골프장 날짜
+        var dateObj = new Date();
+        var year = dateObj.getFullYear();
+        var month = dateObj.getMonth()+1;
+        var date = dateObj.getDate();
+        document.getElementById('date').innerHTML = year + '-' + month + '-' + date;
 });
