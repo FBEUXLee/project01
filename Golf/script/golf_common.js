2c681390d656ce5850669161f03098dda3e46a4b
FBEUXLee <dlwlgur37@gmail.com>
2023-02-08 16:39:38 +0900

    golf

--- Golf/script/golf_common.js
+++ Golf/script/golf_common.js
@@ -1,9 +1,9 @@
 $(function () {
     // nav
-    $('.gnb .depth').hide();
-    $('.gnb>ul>li').hover(function () {
-        $('.gnb .depth').stop().slideToggle();
-    });
+    // $('.gnb .depth').hide();
+    // $('.gnb>ul>li').hover(function () {
+    //     $('.gnb .depth').stop().slideToggle();
+    // });
 
     // 사이트맵
     $('.account .sitemap_btn').click(function () {
@@ -16,7 +16,7 @@ $(function () {
     $('.sitemap .gnb_chor').click(function () {
         $('.sitemap .depth').stop().slideToggle();
         $('.sitemap .gnb_chor span').text(function (e, text) {
-            $(this).parent().parent().siblings().find(".depth").stop().slideToggle("slow", "swing");
+            $(this).children().siblings(".depth").find(".depth>ul").stop().slideToggle("slow", "swing");
             return text === 'expand_Less' ? 'expand_more' : 'expand_Less';
         });
     });
